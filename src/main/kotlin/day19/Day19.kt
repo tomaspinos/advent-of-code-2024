47c83dd87fecd0ff29002dab57711ba25d80acc2
Tomas Pinos <tpinos@cisco.com>
2024-12-19 16:33:55 +0100

    19

---
+++ src/main/kotlin/day19/Day19.kt
@@ -0,0 +1,116 @@
+package day19
+
+import common.resourceFile
+
+fun main() {
+    process("/day19.txt")
+}
+
+fun process(name: String) {
+    val (patterns, words) = readInput(name)
+
+    val engine = Engine()
+
+    for (pattern in patterns) {
+        engine.addPattern(pattern)
+    }
+
+    var matchCount = 0
+    for (word in words) {
+        println(word)
+        val match = engine.matchWord(word)
+        println("  $match")
+        if (match) matchCount++
+    }
+
+    println(matchCount)
+}
+
+fun readInput(name: String): Input {
+    val lines = resourceFile(name).readLines()
+    val patterns = lines.first().split(", ")
+    val words = lines.subList(2, lines.size)
+    return Input(patterns, words)
+}
+
+class Engine {
+    val nodes: MutableMap<Char, Node> = mutableMapOf()
+
+    fun addPattern(pattern: String) {
+        val ch = pattern[0]
+        val matchingNode = nodes[ch]
+        if (matchingNode != null) {
+            matchingNode.addPattern(pattern, 1)
+        } else {
+            val childNode = Node(ch, 1, null, mutableMapOf())
+            nodes[ch] = childNode
+            childNode.addPattern(pattern, 1)
+        }
+    }
+
+    fun findMatches(word: String): List<Node> {
+        val matchingNode = nodes[word[0]]
+        if (matchingNode != null) {
+            return matchingNode.findMatches(word, 1)
+        } else {
+            return listOf()
+        }
+    }
+
+    fun matchWord(word: String): Boolean {
+        val matches = findMatches(word)
+        for (match in matches) {
+            if (match.length == word.length) {
+                return true
+            } else if (matchWord(word.substring(match.length))) {
+                return true
+            }
+        }
+        return false
+    }
+}
+
+data class Node(
+    val ch: Char,
+    val length: Int,
+    var pattern: String? = null,
+    val childNodes: MutableMap<Char, Node>
+) {
+
+    fun addPattern(pattern: String, index: Int) {
+        if (index == pattern.length) {
+            this.pattern = pattern
+        } else {
+            val ch = pattern[index]
+            val matchingNode = childNodes[ch]
+            if (matchingNode != null) {
+                matchingNode.addPattern(pattern, index + 1)
+            } else {
+                val childNode = Node(ch, index + 1, null, mutableMapOf())
+                childNodes[ch] = childNode
+                childNode.addPattern(pattern, index + 1)
+            }
+        }
+    }
+
+    fun findMatches(word: String, index: Int): List<Node> {
+        if (index == word.length) {
+            if (pattern != null) {
+                return listOf(this)
+            }
+        } else {
+            val resultingNodes = mutableListOf<Node>()
+            if (pattern != null) {
+                resultingNodes += this
+            }
+            val matchingNode = childNodes[word[index]]
+            if (matchingNode != null) {
+                resultingNodes += matchingNode.findMatches(word, index + 1)
+            }
+            return resultingNodes
+        }
+        return emptyList()
+    }
+}
+
+data class Input(val patterns: List<String>, val words: List<String>)
---
+++ src/main/kotlin/day19/Day19Tests.kt
@@ -0,0 +1,5 @@
+package day19
+
+fun main() {
+    process("/day19sample1.txt")
+}
