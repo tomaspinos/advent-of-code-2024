a4f2809a6f8272b2329afc11f23e638f1060c543
Tomas Pinos <tpinos@cisco.com>
2024-12-24 11:33:23 +0100

    21 - part 1

--- src/main/kotlin/day21/Day21.kt
+++ src/main/kotlin/day21/Day21.kt
@@ -1,57 +1,101 @@
 package day21
 
 import common.XY
-import day21.DirKey.*
+import day21.Key.*
 
 fun main() {
     computeNumPaths()
     computeDirPaths()
-    part1(
-        listOf(
-            listOf(3, 1, 9, A),
-            listOf(6, 7, 0, A),
-            listOf(3, 4, 9, A),
-            listOf(9, 6, 4, A),
-            listOf(5, 8, 6, A)
-        )
+    val input = listOf(
+        listOf(_3, _1, _9, A),
+        listOf(_6, _7, _0, A),
+        listOf(_3, _4, _9, A),
+        listOf(_9, _6, _4, A),
+        listOf(_5, _8, _6, A)
     )
+    part1(input)
+    //part2(input)
 }
 
-const val A = 10
+fun part1(codes: List<List<Key>>) {
+    val result = codes.sumOf { code ->
+        val pressCount = count(code, 3, numPaths)
+        overallCost(code, pressCount)
+    }
+    println(result)
+}
 
-val numKeys = mapOf(
-    7 to XY(0, 0), 8 to XY(1, 0), 9 to XY(2, 0),
-    4 to XY(0, 1), 5 to XY(1, 1), 6 to XY(2, 1),
-    1 to XY(0, 2), 2 to XY(1, 2), 3 to XY(2, 2),
-    0 to XY(1, 3), A to XY(2, 3)
+fun part2(codes: List<List<Key>>) {
+    val result = codes.sumOf { code ->
+        val pressCount = count(code, 26, numPaths)
+        overallCost(code, pressCount)
+    }
+    println(result)
+}
+
+fun overallCost(code: List<Key>, pressCount: Long): Long {
+    val num = codeToNum(code)
+    println("${num}A: $pressCount * $num")
+    return pressCount * num
+}
+
+val numKeyboard = mapOf(
+    _7 to XY(0, 0), _8 to XY(1, 0), _9 to XY(2, 0),
+    _4 to XY(0, 1), _5 to XY(1, 1), _6 to XY(2, 1),
+    _1 to XY(0, 2), _2 to XY(1, 2), _3 to XY(2, 2),
+    _0 to XY(1, 3), A to XY(2, 3)
 )
 
-val dirKeys = mapOf(
-    UP to XY(1, 0), PRESS to XY(2, 0),
+val dirKeyboard = mapOf(
+    UP to XY(1, 0), A to XY(2, 0),
     LEFT to XY(0, 1), DOWN to XY(1, 1), RIGHT to XY(2, 1)
 )
 
-val numPaths = Array(11) { Array(11) { mutableListOf<Path>() } }
-val dirPaths = Array(5) { Array(5) { mutableListOf<Path>() } }
+val numPaths: MutableMap<Key, MutableMap<Key, MutableList<Path>>> = mutableMapOf()
+val dirPaths: MutableMap<Key, MutableMap<Key, MutableList<Path>>> = mutableMapOf()
+
+fun count(code: List<Key>, level: Int, paths: Map<Key, Map<Key, List<Path>>>): Long {
+    println("$code, $level")
+    if (level == 0) {
+        return code.size.toLong()
+    } else {
+        val keyToKeyTransitions = (listOf(A) + code).zipWithNext()
+        return keyToKeyTransitions.sumOf { (from, to) ->
+            val pathsFromTo = paths[from]!![to]!!
+            pathsFromTo.minOf { path ->
+                count(path.extend(A).keys, level - 1, dirPaths)
+            }
+        }
+    }
+}
+
+fun codeToNum(code: List<Key>): Int {
+    return code.filter { it in setOf(_0, _1, _2, _3, _4, _5, _6, _7, _8, _9) }
+        .map { it.ch }
+        .joinToString("")
+        .toInt()
+}
 
 fun computeNumPaths() {
-    numKeys.keys.forEach(::computeNumPaths)
+    numKeyboard.keys.forEach(::computeNumPaths)
 }
 
 fun computeDirPaths() {
-    dirKeys.keys.forEach(::computeDirPaths)
+    dirKeyboard.keys.forEach(::computeDirPaths)
 }
 
-fun computeNumPaths(num: Int) {
-    val startXY = numKeys[num]!!
+fun computeNumPaths(num: Key) {
+    numPaths[num] = mutableMapOf()
+    numPaths[num]!![num] = mutableListOf(Path(listOf()))
+    val startXY = numKeyboard[num]!!
     val visitedXYs = mutableSetOf<XY>()
     val queue = mutableListOf(startXY to Path(emptyList()))
     while (queue.isNotEmpty()) {
         val (xy, path) = queue.removeFirst()
-        for ((nextNum, nextXY, dirKey) in numKeyNeighbors(xy)) {
+        for ((nextNum, nextXY, throughKey) in numKeyNeighbors(xy)) {
             if (nextXY !in visitedXYs) {
-                val nextPath = path.extend(dirKey)
-                numPaths[num][nextNum] += nextPath
+                val nextPath = path.extend(throughKey)
+                numPaths[num]!!.getOrPut(nextNum) { mutableListOf() } += nextPath
                 queue += nextXY to nextPath
             }
         }
@@ -59,16 +103,18 @@ fun computeNumPaths(num: Int) {
     }
 }
 
-fun computeDirPaths(dirKey: DirKey) {
-    val startXY = dirKeys[dirKey]!!
+fun computeDirPaths(dir: Key) {
+    dirPaths[dir] = mutableMapOf()
+    dirPaths[dir]!![dir] = mutableListOf(Path(listOf()))
+    val startXY = dirKeyboard[dir]!!
     val visitedXYs = mutableSetOf<XY>()
     val queue = mutableListOf(startXY to Path(emptyList()))
     while (queue.isNotEmpty()) {
         val (xy, path) = queue.removeFirst()
-        for ((toDirKey, nextXY, throughDirKey) in dirKeyNeighbors(xy)) {
+        for ((toDir, nextXY, throughKey) in dirKeyNeighbors(xy)) {
             if (nextXY !in visitedXYs) {
-                val nextPath = path.extend(throughDirKey)
-                dirPaths[dirKey.ordinal][toDirKey.ordinal] += nextPath
+                val nextPath = path.extend(throughKey)
+                dirPaths[dir]!!.getOrPut(toDir) { mutableListOf() } += nextPath
                 queue += nextXY to nextPath
             }
         }
@@ -76,36 +122,38 @@ fun computeDirPaths(dirKey: DirKey) {
     }
 }
 
-fun numKeyNeighbors(xy: XY): List<Triple<Int, XY, DirKey>> {
+fun numKeyNeighbors(xy: XY): List<Triple<Key, XY, Key>> {
     return listOf(xy.left() to LEFT, xy.up() to UP, xy.right() to RIGHT, xy.down() to DOWN)
-        .filter { (xy, _) -> numKeys.values.contains(xy) }
-        .map { (xy, dirKey) -> Triple(numKeys.entries.find { it.value == xy }!!.key, xy, dirKey) }
+        .filter { (xy, _) -> numKeyboard.values.contains(xy) }
+        .map { (xy, key) ->
+            Triple(numKeyboard.entries.find { it.value == xy }!!.key, xy, key)
+        }
 }
 
-fun dirKeyNeighbors(xy: XY): List<Triple<DirKey, XY, DirKey>> {
+fun dirKeyNeighbors(xy: XY): List<Triple<Key, XY, Key>> {
     return listOf(xy.left() to LEFT, xy.up() to UP, xy.right() to RIGHT, xy.down() to DOWN)
-        .filter { (xy, _) -> dirKeys.values.contains(xy) }
-        .map { (xy, dirKey) -> Triple(dirKeys.entries.find { it.value == xy }!!.key, xy, dirKey) }
+        .filter { (xy, _) -> dirKeyboard.values.contains(xy) }
+        .map { (xy, dirKey) ->
+            Triple(
+                dirKeyboard.entries.find { it.value == xy }!!.key,
+                xy,
+                dirKey
+            )
+        }
 }
 
-fun part1(codes: List<List<Int>>) {
-    TODO("Not yet implemented")
-}
+class Path(val keys: List<Key>) {
+    fun extend(key: Key): Path = Path(keys + key)
 
-class Path(val dirKeys: List<DirKey>) {
-    fun extend(dirKey: DirKey): Path = Path(dirKeys + dirKey)
-    fun join(other: Path): Path = Path(dirKeys + other.dirKeys)
-    fun print(code: List<Int>) {
-        print("${code.subList(0, 3).joinToString("") { it.toString() } + 'A'}: ")
-        for (dirKey in dirKeys) print(dirKey.ch)
-        println()
-    }
+    fun join(other: Path): Path = Path(keys + other.keys)
 
     override fun toString(): String {
-        return dirKeys.map(DirKey::ch).joinToString("")
+        return keys.map(Key::ch).joinToString("")
     }
 }
 
-enum class DirKey(val ch: Char) {
-    UP('^'), DOWN('v'), LEFT('<'), RIGHT('>'), PRESS('A')
+enum class Key(val ch: Char) {
+    _0('0'), _1('1'), _2('2'), _3('3'), _4('4'), _5('5'), _6('6'), _7('7'), _8('8'), _9('9'),
+    UP('^'), DOWN('v'), LEFT('<'), RIGHT('>'),
+    A('A')
 }
--- src/main/kotlin/day21/Day21Tests.kt
+++ src/main/kotlin/day21/Day21Tests.kt
@@ -1,16 +1,17 @@
 package day21
 
+import day21.Key.*
+
 fun main() {
     computeNumPaths()
-//    val paths = type1(listOf(0, 2, 9, A), 0, A, listOf())
-//    paths.forEach { it.print(listOf(0, 2, 9, A)) }
-//    part1(
-//        listOf(
-//            listOf(0, 2, 9, A),
-//            listOf(9, 8, 0, A),
-//            listOf(1, 7, 9, A),
-//            listOf(4, 5, 6, A),
-//            listOf(3, 7, 9, A)
-//        )
-//    )
+    computeDirPaths()
+    part1(
+        listOf(
+            listOf(_0, _2, _9, A),
+//            listOf(_9, _8, _0, A),
+//            listOf(_1, _7, _9, A),
+//            listOf(_4, _5, _6, A),
+//            listOf(_3, _7, _9, A)
+        )
+    )
 }
